5427fd85575779308dfe70f3e59e157a1056abdf
Aniede Chibundu <ndubest56@gmail.com>
2024-07-20 15:14:34 +0100

    checkup javascript

---
+++ 02-javaScript-website/calc.js
@@ -0,0 +1,15 @@
+let num1 = document.getElementById("num1");
+let num2 = document.getElementById("num2");
+let calc = document.getElementById("calc");
+let result = document.getElementById("result");
+
+let value1 = (num1.value = "Hello");
+let value2 = (num2.value = "A sign");
+result.innerText = "Sign Up";
+
+// calc.onclick((e) => calculate(e, value1, value2));
+
+// function calculate(e, value1, value2) {
+//   e.preventDefault();
+//   console.log(value1, value2);
+// }
