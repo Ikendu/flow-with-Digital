0c474c771f68c7503d687e0740a6f9dd5553c212
Aniede Chibundu <ndubest56@gmail.com>
2024-07-25 21:39:13 +0100

    digital for all backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,31 @@
 const express = require(`express`);
-
 const app = express();
 
+const items = [
+  {
+    id: "asd123",
+    name: "tea",
+  },
+  {
+    id: "qwe234",
+    name: "milk",
+  },
+  {
+    id: "sdf345",
+    name: "garri",
+  },
+  {
+    id: "dfg456",
+    name: "okpa",
+  },
+  {
+    id: "dss432",
+    name: "gala",
+  },
+];
+
 app.get(`/`, (req, res) => {
-  res.send(`Welcome on board`);
+  res.send(items);
 });
 
 app.listen(3000, () => {
