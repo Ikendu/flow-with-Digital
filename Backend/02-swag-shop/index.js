31f0897cbcb5280a909bd87d753c036e99b65cf3
Aniede Chibundu <ndubest56@gmail.com>
2024-07-26 16:13:09 +0100

    digital swag shop

--- Backend/02-swag-shop/index.js
+++ Backend/02-swag-shop/index.js
@@ -20,6 +20,27 @@ app.post(`/product`, (req, res) => {
   let product = new Product();
   product.title = req.body.title;
   product.price = req.body.price;
+
+  product.save((err, savedItem) => {
+    if (err) return res.status(500).json({ error: `Something went wrong` });
+    else return res.status(200).json(savedItem);
+  });
+});
+
+app.get(`/products`, async (req, res) => {
+  //   let allProducts = await Product.find({});
+  //   res.status(200).json(allProducts);
+  //      OR
+
+  Product.find({}, (err, allProducts) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ error: `We could not process your request` });
+    } else {
+      return res.json(allProducts);
+    }
+  });
 });
 
 app.listen(3000, () => console.log(`Connected at port 3000`));
