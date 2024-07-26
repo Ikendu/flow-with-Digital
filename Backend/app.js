dd5e56e8e09d2d5ffe97c8002606be0f1feb3fdb
Aniede Chibundu <ndubest56@gmail.com>
2024-07-26 07:12:54 +0100

    digital for all res api

--- Backend/app.js
+++ Backend/app.js
@@ -36,12 +36,35 @@ app.get(`/`, (req, res) => {
 });
 
 // for posting items to the server
-app.post(`/`, (req, res) => {
+app.post(`/post`, (req, res) => {
   let postItem = req.body;
+  //   error handling
   if (!postItem || !postItem.name)
     return res.status(500).json({ error: `Send a complete object` });
+  // adding the food item into the foodItems array
   foodItems.push(req.body);
   return res.status(200).json(foodItems);
+});
+
+// for updating the food items using the id from the body. Id can also be gotten from params using /update/:id and req.params.id
+app.put(`/update`, (req, res) => {
+  let updateData = req.body.name; //get the item to update
+  let itemId = req.body.id; //get the id of the item from the body
+
+  if (!updateData || updateData == ``) {
+    return res
+      .status(500)
+      .json({ error: `Please specify what you want to update` });
+  }
+  // using loop to get the item from static array. Normally we use .find or .findById to get item id from the database
+  for (let i = 0; i < foodItems.length; i++) {
+    let checkItem = foodItems[i];
+    if (checkItem.id === itemId) {
+      checkItem.name = updateData;
+      return res.status(200).json(foodItems);
+    }
+  }
+  return res.status(500).json({ error: `Item not found` });
 });
 
 app.listen(3000, () => {
